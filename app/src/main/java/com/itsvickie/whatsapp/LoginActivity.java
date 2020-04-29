02966f1e696c364baed864755504d2247d519e75
Vitória Camila <vcamilaxs@gmail.com>
2020-04-29 15:21:37 -0300

    Início do Projeto

---
+++ app/src/main/java/com/itsvickie/whatsapp/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.itsvickie.whatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText userEmail;
+    private EditText userSenha;
+    private Button btnLogin;
+    private Button btnCadastrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        userEmail = findViewById(R.id.txtEmail);
+        userSenha = findViewById(R.id.txtSenha);
+        btnLogin = findViewById(R.id.btnEntrar);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent telaCadastro = new Intent();
+            }
+        });
+    }
+}
